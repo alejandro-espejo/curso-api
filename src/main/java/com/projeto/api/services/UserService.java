b391804e999ec31b6f809a11111cb3142607807e
Alejandro Yujra <alejandro_yujra@hotmail.com>
2023-07-18 21:54:23 -0300

    endpoint findAll para user

--- src/main/java/com/projeto/api/services/UserService.java
+++ src/main/java/com/projeto/api/services/UserService.java
@@ -2,7 +2,11 @@ package com.projeto.api.services;
 
 import com.projeto.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
